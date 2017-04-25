c13b5106aab597d60da0bd2965c77141ab4a8566
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-25 16:15:53 +0100

    spike method to change text on browser page

---
+++ note-controller.js
@@ -0,0 +1,8 @@
+// elem = document.getElementsByClassName('app')[0]
+// elem.innerHTML = 'Goodbye';
+
+
+function changeText() {
+  elem = document.getElementsByClassName('app')[0]
+  elem.innerHTML = 'Goodbye';
+}
--- note-list-model.js
+++ note-list-model.js
@@ -1,16 +1,16 @@
 (function(exports) {
 
   function NoteList() {
-    this._noteArray = []
+    this._noteArray = [];
   }
 
   NoteList.prototype.addNote = function(string) {
     this._noteArray.push(note = new Note(string));
-  }
+  };
 
   NoteList.prototype.printNote = function() {
     return this._noteArray;
-  }
+  };
 
   exports.NoteList = NoteList;
 })(this);
