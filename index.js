cc4baa4d1838e26892dab2d524af08578c15eedc
Jessica Rodriguez <jessicarodriguez@Jessicas-MacBook-Pro.local>
2017-04-25 17:03:50 +0100

    create note controller and indexjs to change html in browser

---
+++ index.js
@@ -0,0 +1,5 @@
+      var noteList = new NoteList();
+      var noteController = new NoteController(noteList);
+
+      noteController.addNotes();
+      noteController.makeView();
--- note-controller.js
+++ note-controller.js
@@ -1,8 +1,27 @@
-// elem = document.getElementsByClassName('app')[0]
-// elem.innerHTML = 'Goodbye';
-
 
 function changeText() {
   elem = document.getElementsByClassName('app')[0]
   elem.innerHTML = 'Goodbye';
 }
+
+(function(exports) {
+
+  function NoteController(noteList) {
+    this.noteList = noteList;
+  }
+
+  NoteController.prototype.addNotes = function() {
+    this.noteList.addNote("Favourite drink: seltzer")
+  }
+
+  NoteController.prototype.makeView = function() {
+    this.noteListView = new ListView(this.noteList);
+  }
+
+  NoteController.prototype.writeHtml = function() {
+    elem = document.getElementsByClassName('app')[0];
+    elem.innerHTML = this.noteListView.list();
+  }
+
+  exports.NoteController = NoteController;
+})(this);
